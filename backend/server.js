60e947bfbd9703b24fb59834f1ed5cef3627e351
itstharun1 <144290544+itstharun1@users.noreply.github.com>
2024-09-25 05:39:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin:["https://deploy-mern-1whq.vercel.app"],
+        methods:["POST","PUT","GET","DELETE"],
+        credentials:true
+    }
+));
 
 // Initialize SQLite database
 const db = new sqlite3.Database('./database.db', (err) => {
